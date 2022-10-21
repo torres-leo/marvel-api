84b4a711880ed01f37974b2b456bfa9570fb2936
leo torres <leojts24@gmail.com>
2022-10-21 11:49:46 -0600

    integrate new api

--- config/axios.js
+++ config/axios.js
@@ -4,14 +4,14 @@ const axiosClient = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-axiosClient.interceptors.request.use((request) => {
-	request.params = {
-		...request.params,
-		ts: 1,
-		apikey: process.env.NEXT_PUBLIC_API_KEY,
-		hash: process.env.NEXT_PUBLIC_API_HASH,
-	};
-	return request;
-});
+// axiosClient.interceptors.request.use((request) => {
+// 	request.params = {
+// 		...request.params,
+// 		ts: 1,
+// 		apikey: process.env.NEXT_PUBLIC_API_KEY,
+// 		hash: process.env.NEXT_PUBLIC_API_HASH,
+// 	};
+// 	return request;
+// });
 
 export default axiosClient;
